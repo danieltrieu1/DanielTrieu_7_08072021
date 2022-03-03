ad6c8440d8087702b81b20e0cda01a513c2e10ab
mac <daniel.trieu1@gmail.com>
2022-03-04 00:12:14 +0100

    commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,11 +3,14 @@ const express = require('express')
 
 const authCtrl = require('../controllers/auth')
 
+const checkPassword = require('../middleware/password-validator');
+const checkEmail = require("../middleware/email-validator");
+
 // Récupération du router
 let router = express.Router()
 
 // Routage ressources Authentication
-router.post('/signup', authCtrl.signup)
+router.post('/signup', checkEmail, checkPassword, authCtrl.signup)
 router.post('/login', authCtrl.login)
 
 module.exports = router
